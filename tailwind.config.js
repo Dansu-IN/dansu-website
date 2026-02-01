44b50cd0a27f21ccf250185023b197d1c0e6a168
Dansu-IN <anand_p_shukla@yahoo.co.in>
2026-02-01 15:05:06 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
